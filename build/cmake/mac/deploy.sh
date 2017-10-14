8009f101ba2dd2344571d84ee6a43d95e07750e5
Michal Duda <github@vookimedlo.cz>
2017-10-14 19:24:10 +0200

    MacOS plugin support

--- build/cmake/mac/deploy.sh
+++ build/cmake/mac/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+target=VookiImageViewer.app/Contents/PlugIns/imageformats/
+
 ~/Qt/5.9.1/clang_64/bin/macdeployqt build/VookiImageViewer.app -always-overwrite
+
+cd build
+cp -f libvooki_kimg_pcx.so libvooki_kimg_pic.so libvooki_kimg_psd.so libvooki_kimg_ras.so libvooki_kimg_rgb.so libvooki_kimg_tga.so libvooki_kimg_xcf.so $target
+cp -f  ../../../../src/plugins/kimageformats/psd.json ../../../../src/plugins/kimageformats/tga.json  ../../../../src/plugins/kimageformats/pcx.json ../../../../src/plugins/kimageformats/ras.json ../../../../src/plugins/kimageformats/xcf.json  ../../../../src/plugins/kimageformats/pic.json ../../../../src/plugins/kimageformats/rgb.json $target
