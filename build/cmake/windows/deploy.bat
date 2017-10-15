284b034922bc34777d5a2a468ba8cb97c5febd6c
Michal Duda <github@vookimedlo.cz>
2017-10-15 13:18:57 +0200

    Windows image plugins deployment support

--- build/cmake/windows/deploy.bat
+++ build/cmake/windows/deploy.bat
@@ -1,2 +1,4 @@
 @echo off
 %QTDIR%\bin\windeployqt.exe --release --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-webkit2 --no-opengl-sw --no-angle build\Release\VookiImageViewer.exe
+
+robocopy build\Release\ build\Release\imageformats\ vooki_*.dll /MOV /IS /IT
