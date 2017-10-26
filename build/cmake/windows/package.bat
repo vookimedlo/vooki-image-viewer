59913231cd3695f15c34cc0f3318ee6586c24ece
Michal Duda <github@vookimedlo.cz>
2017-10-26 23:05:02 +0200

    Initial installer support

---
+++ build/cmake/windows/package.bat
@@ -0,0 +1,8 @@
+@echo off
+
+robocopy build\Release\ "support\package\SourceDir\PFiles\Michal Duda\VookiImageViewer" * /MIR
+
+cd support\package\
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" -arch x64 .\package.xml
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUIExtension  .\package.wixobj
+
