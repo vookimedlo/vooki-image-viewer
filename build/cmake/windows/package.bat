b9ce22071b03c032030dfe487faafd1540e25e51
Michal Duda <github@vookimedlo.cz>
2017-10-27 19:12:56 +0200

    WIX Installer Artwork

--- build/cmake/windows/package.bat
+++ build/cmake/windows/package.bat
@@ -4,5 +4,4 @@ robocopy build\Release\ "support\package\SourceDir\PFiles\Michal Duda\VookiImage
 
 cd support\package\
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" -arch x64 .\package.xml
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUIExtension  .\package.wixobj
-
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -ext WixUIExtension  .\package.wixobj -out VookiImageViewer-x64-win10.msi
