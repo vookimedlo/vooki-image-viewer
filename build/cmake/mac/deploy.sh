ffce7de8b5f58e9802b845ea09ca86eef90ab8de
Michal Duda <github@vookimedlo.cz>
2017-11-24 20:48:05 +0100

    Script for MacOS deployment updated

--- build/cmake/mac/deploy.sh
+++ build/cmake/mac/deploy.sh
@@ -2,7 +2,7 @@
 
 target=VookiImageViewer.app/Contents/PlugIns/imageformats/
 
-~/Qt/5.9.2/clang_64/bin/macdeployqt build/VookiImageViewer.app -always-overwrite
+~/Qt/5.9.3/clang_64/bin/macdeployqt build/VookiImageViewer.app -always-overwrite
 
 cd build
 cp -f libvooki_kimg_pcx.so libvooki_kimg_pic.so libvooki_kimg_psd.so libvooki_kimg_ras.so libvooki_kimg_rgb.so libvooki_kimg_tga.so libvooki_kimg_xcf.so $target
