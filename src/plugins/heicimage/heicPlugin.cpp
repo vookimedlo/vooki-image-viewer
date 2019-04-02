c835107809a1958150d681ba2c356bf0a435b4a7
Michal Duda <github@vookimedlo.cz>
2019-04-02 22:20:12 +0200

    Libs: HeicImage Qt image plugin added
    - based on the https://github.com/jakar/qt-heif-image-plugin/releases/tag/0.3.3

---
+++ src/plugins/heicimage/heicPlugin.cpp
@@ -0,0 +1,58 @@
+/****************************************************************************
+VookiImageViewer - tool to showing images.
+Copyright(C) 2019  Michal Duda <github@vookimedlo.cz>
+
+https://github.com/vookimedlo/vooki-image-viewer
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+#include "heicHandler.h"
+#include "heicPlugin.h"
+
+QImageIOPlugin::Capabilities HeicPlugin::capabilities(QIODevice *device, const QByteArray &format) const
+{
+    static const QStringList formats = { "heic" };
+
+    if (formats.contains(format))
+    {
+        return { CanRead };
+    }
+
+    if (!format.isEmpty())
+    {
+        return nullptr;
+    }
+
+    if (!device || !device->isOpen())
+    {
+        return nullptr;
+    }
+
+    Capabilities capabilities;
+    if (device->isReadable() && HeicHandler::canRead(device))
+    {
+        capabilities |= CanRead;
+    }
+
+    return capabilities;
+}
+
+QImageIOHandler *HeicPlugin::create(QIODevice *device, const QByteArray &format) const
+{
+    QImageIOHandler *handler = new HeicHandler();
+    handler->setDevice(device);
+    handler->setFormat(format);
+    return handler;
+}
---
+++ src/plugins/heicimage/heicPlugin.h
@@ -0,0 +1,32 @@
+#pragma once
+/****************************************************************************
+VookiImageViewer - tool to showing images.
+Copyright(C) 2019  Michal Duda <github@vookimedlo.cz>
+
+https://github.com/vookimedlo/vooki-image-viewer
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+#include <QImageIOPlugin>
+
+class HeicPlugin : public QImageIOPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QImageIOHandlerFactoryInterface" FILE "heic.json")
+
+public:
+    Capabilities capabilities(QIODevice *device, const QByteArray &format) const Q_DECL_OVERRIDE;
+    QImageIOHandler *create(QIODevice *device, const QByteArray &format = QByteArray()) const Q_DECL_OVERRIDE;
+};
