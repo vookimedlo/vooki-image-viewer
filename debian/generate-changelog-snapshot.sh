e0a256cabaffaa14868eabcef0ff4788230633bc
Michal Duda <github@vookimedlo.cz>
2019-04-20 22:49:06 +0200

    Debian: Changelog generation

---
+++ debian/generate-changelog-snapshot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+tag=`git tag -l v* | sort -V | tail -1`
+[ `git log --exit-code $tag..HEAD | wc -l` -ne 0 ] && gbp dch -s $tag -S --no-multimaint --nmu --ignore-branch --snapshot-number="'{:%Y%m%d%H%M%S}'.format(__import__('datetime').datetime.fromtimestamp(`git log -1 --pretty=format:%at`))"
