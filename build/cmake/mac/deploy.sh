55db61ee51874596d199456104b31f4ffcc58ff0
Michal Duda <github@vookimedlo.cz>
2019-11-02 19:06:32 +0100

    🔩Build: Radiance HDR [MacOS]

--- build/cmake/mac/deploy.sh
+++ build/cmake/mac/deploy.sh
@@ -5,5 +5,5 @@ target=VookiImageViewer.app/Contents/PlugIns/imageformats/
 ~/Qt/5.13.2/clang_64/bin/macdeployqt build/VookiImageViewer.app -always-overwrite
 
 cd build
-cp -f libvooki_kimg_pcx.so libvooki_kimg_pic.so libvooki_kimg_psd.so libvooki_kimg_ras.so libvooki_kimg_rgb.so libvooki_kimg_tga.so libvooki_kimg_xcf.so $target
+cp -f libvooki_kimg_hdr.so libvooki_kimg_pcx.so libvooki_kimg_pic.so libvooki_kimg_psd.so libvooki_kimg_ras.so libvooki_kimg_rgb.so libvooki_kimg_tga.so libvooki_kimg_xcf.so $target
 cp -f libvooki_raw_thumb.so $target
