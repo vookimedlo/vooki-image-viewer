c01aecb683f2956ae06b18fb5e9eae28ee3cf5e5
Michal Duda <github@vookimedlo.cz>
2020-12-08 23:05:05 +0100

    🏗Build: Qt6 build support [Windows]

--- build/cmake/windows/deploy-release.bat
+++ build/cmake/windows/deploy-release.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /D "%~dp0"
 
-%QTDIR%\bin\windeployqt.exe --release --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-webkit2 --no-opengl-sw --no-angle build\Release\VookiImageViewer.exe
+%QTDIR%\bin\windeployqt.exe --release --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw build\Release\VookiImageViewer.exe
 
 (robocopy build\Release\ build\Release\imageformats\ vooki_*.dll /MOV /IS /IT)
 IF %%ERRORLEVEL%% GEQ 4 EXIT /b %%ERRORLEVEL%%
