972ff6502dae0e81b7257dbd55b14b60caa94882
Michal Duda <github@vookimedlo.cz>
2020-12-10 23:52:06 +0100

    🏗Build: Deployment script updated [windows]

--- build/cmake/windows/deploy-release.bat
+++ build/cmake/windows/deploy-release.bat
@@ -3,6 +3,13 @@ cd /D "%~dp0"
 
 %QTDIR%\bin\windeployqt.exe --release --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw build\Release\VookiImageViewer.exe
 
+FOR /F "tokens=* USEBACKQ" %%F IN (`%QTDIR%\bin\qtpaths.exe --qt-version`) DO (
+SET QT_VERSION_DEV=%%F
+)
+
+(robocopy ..\prebuilt\qtimageformats\windows\%QT_VERSION_DEV%\ build\Release\imageformats\ *.dll /IS /IT)
+IF %%ERRORLEVEL%% GEQ 4 EXIT /b %%ERRORLEVEL%%
+
 (robocopy build\Release\ build\Release\imageformats\ vooki_*.dll /MOV /IS /IT)
 IF %%ERRORLEVEL%% GEQ 4 EXIT /b %%ERRORLEVEL%%
 
