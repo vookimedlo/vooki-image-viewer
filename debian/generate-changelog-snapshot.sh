483e29ccf3f12ed2b4a1d9d11f1b7056f0f50354
Michal Duda <github@vookimedlo.cz>
2022-10-11 22:28:28 +0200

    🏗Build: [Unix] Obsolete DEB and RPM packaging files removed.

--- debian/generate-changelog-snapshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-tag=`git tag -l v* | sort -V | tail -1`
-[ `git log --exit-code $tag..HEAD | wc -l` -ne 0 ] && gbp dch -s $tag -S --no-multimaint --nmu --ignore-branch --snapshot-number="'{:%Y%m%d%H%M%S}'.format(__import__('datetime').datetime.fromtimestamp(`git log -1 --pretty=format:%at`))"
-
-sed -i 's/^\(vookiimageviewer [^)]\+\)/\1-1/' debian/changelog
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vookiimageviewer):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vookiimageviewer" command="/usr/bin/vookiimageviewer"
