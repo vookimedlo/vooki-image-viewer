84a703775b5630fd3a713c025a1a2a8a0b3dc8ba
Michal Duda <github@vookimedlo.cz>
2023-09-02 22:38:02 +0200

    🧪Test: Initial ImageRotationTest.

---
+++ src/processing/transformation/test/ImageRotationTest.cpp
@@ -0,0 +1,100 @@
+/****************************************************************************
+VookiImageViewer - a tool for showing images.
+- https://github.com/vookimedlo/vooki-image-viewer
+
+SPDX-FileCopyrightText: 2023 Michal Duda <github@vookimedlo.cz>
+SPDX-License-Identifier: GPL-3.0-or-later
+SPDX-FileType: SOURCE
+
+****************************************************************************/
+
+#include <QTest>
+#include <QTransform>
+
+#include "../ImageRotation.h"
+
+class ImageRotationTest: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void rotateLeft4x();
+    void rotateRight4x();
+    void rotateLeftRight();
+    void rotateRightLeft();
+    void resetProperties();
+};
+
+void ImageRotationTest::rotateLeft4x()
+{
+    ImageRotation<QTransform> m_imageRotation {};
+    auto transformBefore =  m_imageRotation.transform().value<QTransform>();
+
+    for (int i = 0; i < 4; ++i )
+    {
+        m_imageRotation.rotateLeft();
+
+        const auto transformAfter =  m_imageRotation.transform().value<QTransform>();
+        QCOMPARE(transformAfter, transformBefore.rotate(-90));
+    }
+}
+
+void ImageRotationTest::rotateRight4x()
+{
+    ImageRotation<QTransform> m_imageRotation {};
+    auto transformBefore =  m_imageRotation.transform().value<QTransform>();
+
+    for (int i = 0; i < 4; ++i )
+    {
+        m_imageRotation.rotateRight();
+
+        const auto transformAfter =  m_imageRotation.transform().value<QTransform>();
+        QCOMPARE(transformAfter, transformBefore.rotate(90));
+    }
+}
+
+void ImageRotationTest::rotateLeftRight()
+{
+    ImageRotation<QTransform> m_imageRotation {};
+    const auto transformBefore =  m_imageRotation.transform().value<QTransform>();
+
+    m_imageRotation.rotateLeft();
+    m_imageRotation.rotateRight();
+
+    const auto transformAfter =  m_imageRotation.transform().value<QTransform>();
+
+    QCOMPARE(transformAfter.isRotating(), false);
+    QCOMPARE(transformAfter, transformBefore);
+}
+
+void ImageRotationTest::rotateRightLeft()
+{
+    ImageRotation<QTransform> m_imageRotation {};
+    const auto transformBefore =  m_imageRotation.transform().value<QTransform>();
+
+    m_imageRotation.rotateRight();
+    m_imageRotation.rotateLeft();
+
+    const auto transformAfter =  m_imageRotation.transform().value<QTransform>();
+
+    QCOMPARE(transformAfter.isRotating(), false);
+    QCOMPARE(transformAfter, transformBefore);
+}
+
+void ImageRotationTest::resetProperties()
+{
+    ImageRotation<QTransform> m_imageRotation {};
+    const auto transformBefore =  m_imageRotation.transform().value<QTransform>();
+
+    m_imageRotation.rotateRight();
+    m_imageRotation.rotateRight();
+    m_imageRotation.resetProperties();
+
+    const auto transformAfter =  m_imageRotation.transform().value<QTransform>();
+
+    QCOMPARE(transformAfter.isRotating(), false);
+    QCOMPARE(transformAfter, transformBefore);
+}
+
+QTEST_MAIN(ImageRotationTest)
+#include "ImageRotationTest.moc"
