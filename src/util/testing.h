3602ca8f4cfd5d7f848ef7792fb090b3ba2dc4b9
Michal Duda <github@vookimedlo.cz>
2023-09-16 21:00:13 +0200

    🧪Test: Static usage.

--- src/util/testing.h
+++ src/util/testing.h
@@ -15,7 +15,7 @@ void runTests(int argc, char* argv[], int* status)
 {
     ::QTest::Internal::callInitMain<TestClass>();
     QApplication app(argc, argv);
-    app.setAttribute(Qt::AA_Use96Dpi, true);
+    QApplication::setAttribute(Qt::AA_Use96Dpi, true);
     QTEST_DISABLE_KEYPAD_NAVIGATION TestClass tc;
     QTEST_SET_MAIN_SOURCE_PATH
     *status |= QTest::qExec(&tc, argc, argv);
