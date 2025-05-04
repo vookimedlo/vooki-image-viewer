61c6fc4f97d794449b038981b26fa2ba9b2350d8
Michal Duda <github@vookimedlo.cz>
2025-05-04 19:16:31 +0200

    🏗Build: GitHub Actions [Windows] - use Windows ARM native runner.

--- build/cmake/platform/windows/WindeployQt.cmake
+++ build/cmake/platform/windows/WindeployQt.cmake
@@ -8,12 +8,21 @@
 #############################################################################
 
 function(deployqt)
-    get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
-    get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-    find_program(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+    GET_TARGET_PROPERTY(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
+    GET_FILENAME_COMPONENT(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+    FIND_PROGRAM(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}" REQUIRED)
 
-    add_custom_command(TARGET ${APPLICATION_NAME} POST_BUILD
+    SET(WINDEPLOYQT_EXECUTABLE "${DEPLOYQT_EXECUTABLE}" PARENT_SCOPE)
+
+    SET(DEPLOYQT_QTPATH_LOCATION "${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/qtpaths.bat")
+    IF (EXISTS "${DEPLOYQT_QTPATH_LOCATION}")
+        SET(DEPLOYQT_QTPATH_OPTION "--qtpaths ${DEPLOYQT_QTPATH_LOCATION}" PARENT_SCOPE)
+    ELSE()
+        SET(DEPLOYQT_QTPATH_OPTION "" PARENT_SCOPE)
+    ENDIF()
+
+    ADD_CUSTOM_COMMAND(TARGET ${APPLICATION_NAME} POST_BUILD
             COMMAND "${CMAKE_COMMAND}" -E
-            env PATH="${_qt_bin_dir}" "${DEPLOYQT_EXECUTABLE}" --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw "$<TARGET_FILE:${APPLICATION_NAME}>"
+            env PATH="${_qt_bin_dir}" "${DEPLOYQT_EXECUTABLE}" ${DEPLOYQT_QTPATH_OPTION} --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw "$<TARGET_FILE:${APPLICATION_NAME}>"
             COMMENT "Deploying QT runtime ...")
 endfunction()
