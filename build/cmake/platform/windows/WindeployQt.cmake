fa778e6ba8fefed809ae0deb2662e431c7beed5f
Michal Duda <github@vookimedlo.cz>
2025-05-16 18:03:56 +0200

    🏗Build: GitHub Actions [Windows] - use Windows ARM native runner.

--- build/cmake/platform/windows/WindeployQt.cmake
+++ build/cmake/platform/windows/WindeployQt.cmake
@@ -8,22 +8,23 @@
 #############################################################################
 
 function(deployqt)
-    get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
-    get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-    find_program(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
+    GET_TARGET_PROPERTY(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
+    GET_FILENAME_COMPONENT(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+
+    FILE(STRINGS "${_qt_bin_dir}/target_qt.conf" host_prefix_pair REGEX '^HostPrefix=')
+    STRING(REGEX REPLACE '^HostPrefix=' '' host_prefix host_prefix_pair)
+    FIND_PROGRAM(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}/${host_prefix}")
 
     if (DEPLOYQT_EXECUTABLE)
-        add_custom_command(TARGET ${APPLICATION_NAME} POST_BUILD
-                COMMAND "${CMAKE_COMMAND}" -E
-                env PATH="${_qt_bin_dir}" "${DEPLOYQT_EXECUTABLE}" --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw "$<TARGET_FILE:${APPLICATION_NAME}>"
-                COMMENT "Deploying QT runtime ...")
-    else ()
-        FILE(STRINGS "${_qt_bin_dir}/target_qt.conf" host_prefix_pair REGEX '^HostPrefix=')
-        STRING(REGEX REPLACE '^HostPrefix=' '' host_prefix host_prefix_pair)
-        find_program(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}/${host_prefix}" REQUIRED)
-        add_custom_command(TARGET ${APPLICATION_NAME} POST_BUILD
+        ADD_CUSTOM_COMMAND(TARGET ${APPLICATION_NAME} POST_BUILD
                 COMMAND "${CMAKE_COMMAND}" -E
                 env PATH="${_qt_bin_dir}/${host_prefix}" "${DEPLOYQT_EXECUTABLE}" --qtpaths "${_qt_bin_dir}/qtpaths.bat" --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw "$<TARGET_FILE:${APPLICATION_NAME}>"
+                COMMENT "Deploying QT runtime ...")
+    else ()
+        FIND_PROGRAM(DEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}" REQUIRED)
+        ADD_CUSTOM_COMMAND(TARGET ${APPLICATION_NAME} POST_BUILD
+                COMMAND "${CMAKE_COMMAND}" -E
+                env PATH="${_qt_bin_dir}" "${DEPLOYQT_EXECUTABLE}" --no-quick-import --no-system-d3d-compiler --compiler-runtime --no-opengl-sw "$<TARGET_FILE:${APPLICATION_NAME}>"
                 COMMENT "Deploying QT runtime ...")
     endif ()
 endfunction()
